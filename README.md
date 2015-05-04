46e65ee25fc73fbda8704f6e7a097c7c22696fc5
Stefan Bruggmann <stefan.bruggmann@dotpulse.ch>
2015-05-04 16:18:41 +0200

    Added the UberspaceDeployment example script

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # TYPO3.Surf.Deployments
+
+TYPO3 Surf Deployment example scripts
+
+## You can use it if you..
+
+* have packages that are not managed by composer
+* don't use the default TYPO3 Neos Distribution filestructure
+* want use rsync
+
+## How it works..
+
+* setup the public-/private-key stuff
+* copy your desired *Deployment.php file to 'FLOW_PATH_ROOT/Build/Surf/'
+* add your settings for
+  * `$domain`
+  * `$username`
+  * `$hostname`
+  * `$sitePackageKey`
+  * `$copyPackages`
+* create a symlink from '/var/www/virtual/[username]/html' to '[deploymentPath]/release/current/Web'
+* execute `./flow surf:deploy YourDesiredDeployment.php`
+
+## Example Uberspace Configuration..
+
+```php
+$domain				= 'dotpulse.ch';
+$username			= 'dotusr';
+$hostname			= 'regulus.uberspace.de';
+$sitePackageKey		= 'Dotpulse.Theme';
+$copyPackages		= array(
+	'Plugins'		=> array( 'Dotpulse.Base' ),
+	'Sites'			=> array( $sitePackageKey )
+);
+```
+
+## What's next..
+
+* adding examples for other servers
---
+++ UberspaceDeployment.php
@@ -0,0 +1,95 @@
+<?php
+
+
+$domain				= '';		// domain.com
+$username			= '';		// usrnam
+$password			= NULL;		// don't use it
+$hostname			= '';		// e.g. server.uberspace.de not username.server.uberspace.de
+$sitePackageKey		= '';		// MyCustom.ThemePackage
+$setFlowRootpath	= false;	// enable if you get internal server erros
+$copyPackages		= array(	// the packages that are not managed by composer
+	'Plugins'		=> array(  ),
+	'Sites'			=> array( $sitePackageKey )
+);
+
+
+// ------------------------------------------------------------------
+
+$domain				= $domain.'.surf';
+$projectKey			= preg_replace("/[^a-zA-Z0-9]+/", "", $domain);
+
+// Create a simple workflow based on the predefined 'SimpleWorkflow'.
+$workflow = new \TYPO3\Surf\Domain\Model\SimpleWorkflow();
+$workflow->setEnableRollback(TRUE);
+
+// Create and configure a simple shell task to add the FLOW_CONTEXT and FLOW_ROOTPATH to your .htaccess file
+$workflow->defineTask($projectKey.':editHtaccess', 'typo3.surf:shell', array(
+	'command' => 'echo -e "\n'
+				 . 'SetEnv FLOW_CONTEXT Production \n'
+				 . ($setFlowRootpath?'SetEnv FLOW_ROOTPATH {deploymentPath}/releases/current/ \n':'')
+				 . '" >> {releasePath}/Web/.htaccess'
+));
+$workflow->addTask($projectKey.':editHtaccess', 'finalize');
+
+
+// Change composer.json to our own and copy some unpacked sources.
+$workflow->defineTask($projectKey.':fixcomposer', 'typo3.surf:localshell', array(
+	'command' => 'cp '.FLOW_PATH_ROOT.'composer.* '.FLOW_PATH_ROOT.'Data/Surf/UberspaceDeployment/'.$domain.'/;'
+));
+$workflow->afterTask('typo3.surf:package:git', $projectKey.':fixcomposer');
+
+// Add missing files that are not managed by composer.
+$addPackages = '';
+foreach ($copyPackages as $folder => $packages) {
+	$addPackages .= 'mkdir -p '.FLOW_PATH_ROOT.'Data/Surf/UberspaceDeployment/'.$domain.'/Packages/'.$folder.'/;';
+	foreach ($packages as $package) {
+		$addPackages .= 'cp -r '.FLOW_PATH_ROOT.'Packages/'.$folder.'/'.$package.' '.FLOW_PATH_ROOT.'Data/Surf/UberspaceDeployment/'.$domain.'/Packages/'.$folder.'/;';
+	}
+}
+$workflow->defineTask($projectKey.':injectfiles', 'typo3.surf:localshell', array(
+	'command' => 'mkdir -p '.FLOW_PATH_ROOT.'Data/Surf/UberspaceDeployment/'.$domain.'/Packages/;'
+				 . 'cp -r '.FLOW_PATH_ROOT.'Configuration '.FLOW_PATH_ROOT.'Data/Surf/UberspaceDeployment/'.$domain.'/Packages/;'
+				 . 'rsync -a --exclude=index.php --exclude=_Resources '.FLOW_PATH_ROOT.'Web/* '.FLOW_PATH_ROOT.'Data/Surf/UberspaceDeployment/'.$domain.'/Web/;'
+				 . $addPackages
+));
+$workflow->beforeTask('typo3.surf:transfer:rsync', $projectKey.':injectfiles');
+
+// Kill running PHP processes.
+$workflow->defineTask($projectKey.':killphp', 'typo3.surf:shell', array(
+	'command' => 'killall -q php-cgi || true;'
+));
+$workflow->afterTask('typo3.surf:symlinkrelease', $projectKey.':killphp');
+
+// Add the workflow to the deployment. The $deployment instance is created by Surf.
+$deployment->setWorkflow($workflow);
+
+// Create and configure your node / nodes (host / hosts).
+$node = new \TYPO3\Surf\Domain\Model\Node('uberspace');
+$node->setHostname($username.'.'.$hostname);
+if ( !is_null($password) ) {
+	$node->setOption('password', $password);
+}
+$node->setOption('username', $username);
+
+// Define your application and add it to your node.
+$application = new \TYPO3\Surf\Application\TYPO3\Flow($domain);
+// At uberspace: create a symlink from '/var/www/virtual/[user]/html' to '[deploymentPath]/release/current/Web'
+$application->setDeploymentPath('/var/www/virtual/'.$username.'/'.$domain);
+$application->setOption('repositoryUrl', 'https://git.typo3.org/Neos/Distributions/Base.git');
+$application->setOption('composerCommandPath', 'composer');
+$application->setOption('keepReleases', '5');
+
+$application->setOption('packageMethod', 'git');
+$application->setOption('transferMethod', 'rsync');
+$application->setOption('updateMethod', NULL);
+$application->setOption('sitePackageKey', $sitePackageKey);
+
+$application->addNode($node);
+
+// remove unused task.
+$deployment->onInitialize(function() use ($workflow, $application) {
+    $workflow->removeTask('typo3.surf:typo3:flow:setfilepermissions');
+});
+
+// Add the application to your deployment.
+$deployment->addApplication($application);
