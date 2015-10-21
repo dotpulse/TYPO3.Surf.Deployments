7a04b69b1395fd874dc360e2520f0076bde7040f
Jon Uhlmann <me@jonnitto.ch>
2015-10-21 21:17:34 +0200

    Update for Neos 2.0

--- Nine.php
+++ Nine.php
@@ -11,10 +11,9 @@ $copyPackages      = array(   // the packages that are not managed by composer
 	'Sites'   => array( $sitePackageKey )
 );
 
-
 // ------------------------------------------------------------------
 
-$domain    = $domain.'.surf';
+$domain     = $domain.'.surf';
 $projectKey = preg_replace("/[^a-zA-Z0-9]+/", "", $domain);
 
 // Create a simple workflow based on the predefined 'SimpleWorkflow'.
@@ -27,6 +26,14 @@ $workflow->defineTask($projectKey.':runHelp', 'typo3.surf:shell', array(
 ));
 $workflow->beforeTask('typo3.surf:typo3:flow:migrate', $projectKey.':runHelp');
 
+// Workaround: Publish images in _Resources/Persistent folder
+$workflow->defineTask($projectKey.':publishImages', 'typo3.surf:shell', array(
+	'command' => 'FLOW_CONTEXT=Production {releasePath}/flow media:clearthumbnails',
+	'command' => 'FLOW_CONTEXT=Production {releasePath}/flow resource:clean',
+	'command' => 'FLOW_CONTEXT=Production {releasePath}/flow resource:publish'
+));
+$workflow->afterTask('typo3.surf:typo3:flow:migrate', $projectKey.':publishImages');
+
 // Create and configure a simple shell task to add the FLOW_CONTEXT and FLOW_ROOTPATH to your .htaccess file
 $workflow->defineTask($projectKey.':editHtaccess', 'typo3.surf:shell', array(
 	'command' => 'echo -e "\n'
@@ -36,25 +43,28 @@ $workflow->defineTask($projectKey.':editHtaccess', 'typo3.surf:shell', array(
 ));
 $workflow->addTask($projectKey.':editHtaccess', 'finalize');
 
-
 // Change composer.json to our own and copy some unpacked sources.
 $workflow->defineTask($projectKey.':fixcomposer', 'typo3.surf:localshell', array(
-	'command' => 'cp '.FLOW_PATH_ROOT.'composer.* '.FLOW_PATH_ROOT.'Data/Surf/NineDeployment/'.$domain.'/;'
+	'command' => 'cp '.FLOW_PATH_ROOT.'composer.* '.FLOW_PATH_ROOT.'Data/Surf/Nine/'.$domain.'/;'
 ));
 $workflow->afterTask('typo3.surf:package:git', $projectKey.':fixcomposer');
 
 // Add missing files that are not managed by composer.
 $addPackages = '';
 foreach ($copyPackages as $folder => $packages) {
-	$addPackages .= 'mkdir -p '.FLOW_PATH_ROOT.'Data/Surf/NineDeployment/'.$domain.'/Packages/'.$folder.'/;';
+	$addPackages .= 'mkdir -p '.FLOW_PATH_ROOT.'Data/Surf/Nine/'.$domain.'/Packages/'.$folder.'/;';
 	foreach ($packages as $package) {
-		$addPackages .= 'cp -r '.FLOW_PATH_ROOT.'Packages/'.$folder.'/'.$package.' '.FLOW_PATH_ROOT.'Data/Surf/NineDeployment/'.$domain.'/Packages/'.$folder.'/;';
+		$addPackages .= 'cp -r '.FLOW_PATH_ROOT.'Packages/'.$folder.'/'.$package.' '.FLOW_PATH_ROOT.'Data/Surf/Nine/'.$domain.'/Packages/'.$folder.'/;';
 	}
 }
 $workflow->defineTask($projectKey.':injectfiles', 'typo3.surf:localshell', array(
-	'command' => 'mkdir -p '.FLOW_PATH_ROOT.'Data/Surf/NineDeployment/'.$domain.'/Packages/;'
-				 . 'cp -Lr '.FLOW_PATH_ROOT.'Configuration '.FLOW_PATH_ROOT.'Data/Surf/NineDeployment/'.$domain.'/;'
-				 . 'rsync -a --exclude=index.php --exclude=_Resources '.FLOW_PATH_ROOT.'Web/* '.FLOW_PATH_ROOT.'Data/Surf/NineDeployment/'.$domain.'/Web/;'
+	'command' => 'rm -rf '.FLOW_PATH_ROOT.'Data/Surf/Nine/'.$domain.'/Packages/;'
+				 . 'mkdir -p '.FLOW_PATH_ROOT.'Data/Surf/Nine/'.$domain.'/Packages/;'
+				 . 'cp -Lr '.FLOW_PATH_ROOT.'Configuration '.FLOW_PATH_ROOT.'Data/Surf/Nine/'.$domain.'/;'
+				 . 'cp -f '.FLOW_PATH_ROOT.'Web/.htaccess '.FLOW_PATH_ROOT.'Data/Surf/Nine/'.$domain.'/Web/.htaccess;'
+				 . 'cp -f '.FLOW_PATH_ROOT.'Web/robots.txt '.FLOW_PATH_ROOT.'Data/Surf/Nine/'.$domain.'/Web/robots.txt;'
+				 . 'rsync -a --ignore-errors '.FLOW_PATH_ROOT.'Packages/Sites/'.$sitePackageKey.'/Resources/Private/WebRoot/* '.FLOW_PATH_ROOT.'Data/Surf/Nine/'.$domain.'/Web/;'
+				 . 'rsync -a --exclude=index.php --exclude=_Resources --exclude=robots.txt '.FLOW_PATH_ROOT.'Web/* '.FLOW_PATH_ROOT.'Data/Surf/Nine/'.$domain.'/Web/;'
 				 . $addPackages
 ));
 $workflow->beforeTask('typo3.surf:transfer:rsync', $projectKey.':injectfiles');
